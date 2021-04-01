ff2d3d58f1c2d2f513ab658c2d39ed3525157855
vashu sharma <vashusharma2625@gmail.com>
2021-04-01 23:18:37 +0530

    Event Listener

---
+++ src/AppBundle/EventListener/DateListener.php
@@ -0,0 +1,27 @@
+<?php
+    namespace AppBundle\EventListener;
+    
+    use Pimcore\Event\Model\DataObjectEvent;
+    use Pimcore\Model\DataObject\Product;
+
+    class DateListener {
+
+        /**
+        * @param DataObjectEvent $e 
+        */
+     
+        
+    
+
+        public function DateException (DataObjectEvent $e) {
+       
+      
+            if ($e->getObject() instanceof Product) {
+                $prod = $e->getObject();
+                if($prod->getLaunchDate() <  date("Y-m-d") ) {
+                    throw new \Pimcore\Model\Element\ValidationException("Product has been expired");
+                    
+                }
+            }
+        }
+    }
